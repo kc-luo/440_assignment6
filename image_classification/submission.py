2eca0f70dd61c26efdb4192bea14a17dce2ed8f0
kc-luo <luokaichun@outlook.com>
2021-11-17 16:58:51 -0600

    Finished problem 3

--- image_classification/submission.py
+++ image_classification/submission.py
@@ -1,10 +1,11 @@
 import collections
 import numpy as np
 
+
 ############################################################
 # Problem 4.1
 
-def runKMeans(k,patches,maxIter):
+def runKMeans(k, patches, maxIter):
     """
     Runs K-means to learn k centroids, for maxIter iterations.
     
@@ -20,34 +21,25 @@ def runKMeans(k,patches,maxIter):
     # with patchSize rows and k columns. Each column is a centroid.
     centroids = np.random.randn(patches.shape[0], k)
     numPatches = patches.shape[1]
-
+    p = patches.transpose()
+    c = centroids.transpose()
     for i in range(maxIter):
         # BEGIN_YOUR_CODE (around 19 lines of code expected)
-        p_expanded = np.repeat(patches[:, np.newaxis, :], k, axis=1)
-        c_expanded = np.repeat(centroids[:, :, np.newaxis], numPatches, axis=2)
-        dist = (p_expanded - c_expanded) ** 2
-        idx = np.argmin(dist, axis=1)
-        idx = np.transpose(idx)
-        idx_supp = np.eye(k)[idx]
-        print(idx_supp.shape)
-
+        dist = (p[:, np.newaxis, :] - c[np.newaxis, :, :]) ** 2
+        idx = np.argmin(np.sum(dist, axis=2), axis=1)
+        idx_supp = np.transpose(np.eye(k)[idx])
         supp_count = np.sum(idx_supp, axis=1)
-        print(supp_count)
-        result = np.repeat(patches[np.newaxis, :], k, axis=0) * idx_supp[:, :, np.newaxis]
 
-        ################################################################################
-        #             END OF YOUR CODE                                                 #
-        ################################################################################
-        #     return centroids
-        centroids = np.sum(result.transpose(0, 2, 1), axis=2) / supp_count[:, np.newaxis]
+        result = p[np.newaxis, :] * idx_supp[:, :, np.newaxis]
+        c = np.sum(result.transpose(0, 2, 1), axis=2) / supp_count[:, np.newaxis]
         # END_YOUR_CODE
+    return c.transpose()
 
-    return centroids
 
 ############################################################
 # Problem 4.2
 
-def extractFeatures(patches,centroids):
+def extractFeatures(patches, centroids):
     """
     Given patches for an image and a set of centroids, extracts and return
     the features for that image.
@@ -62,18 +54,23 @@ def extractFeatures(patches,centroids):
     """
     k = centroids.shape[1]
     numPatches = patches.shape[1]
-    features = np.empty((numPatches,k))
-
+    # features = np.empty((numPatches, k))
     # BEGIN_YOUR_CODE (around 9 lines of code expected)
-    raise Exception("Not yet implemented")
+    p = patches.transpose()
+    c = centroids.transpose()
+    dist = np.sqrt(((p[:, np.newaxis, :] - c[np.newaxis, :, :]) ** 2).sum(axis=2))
+    features = np.maximum(np.mean(dist, axis=-1)[:, np.newaxis] - dist, 0)
     # END_YOUR_CODE
     return features
+
 
 ############################################################
 # Problem 4.3.1
 
 import math
-def logisticGradient(theta,featureVector,y):
+
+
+def logisticGradient(theta, featureVector, y):
     """
     Calculates and returns gradient of the logistic loss function with
     respect to parameter vector theta.
@@ -87,13 +84,15 @@ def logisticGradient(theta,featureVector,y):
       1D numpy array of gradient of logistic loss w.r.t. to theta
     """
     # BEGIN_YOUR_CODE (around 2 lines of code expected)
-    raise Exception("Not yet implemented.")
+    e = math.exp(-np.sum(theta*featureVector) * (2*y-1))
+    return - e / (1+e) * (2*y-1) * featureVector
     # END_YOUR_CODE
+
 
 ############################################################
 # Problem 4.3.2
-    
-def hingeLossGradient(theta,featureVector,y):
+
+def hingeLossGradient(theta, featureVector, y):
     """
     Calculates and returns gradient of hinge loss function with
     respect to parameter vector theta.
@@ -107,6 +106,9 @@ def hingeLossGradient(theta,featureVector,y):
       1D numpy array of gradient of hinge loss w.r.t. to theta
     """
     # BEGIN_YOUR_CODE (around 6 lines of code expected)
-    raise Exception("Not yet implemented.")
+    coeff = (2*y - 1) * featureVector
+    if np.sum(theta*coeff) > 1:
+        return np.zeros(featureVector.shape)
+    else:
+        return - coeff
     # END_YOUR_CODE
-
